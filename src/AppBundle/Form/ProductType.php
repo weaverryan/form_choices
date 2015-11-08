72e7c51b3f16fa866af2bed98d45faf6c0b7e1e9
Ryan Weaver <ryan@thatsquality.com>
2015-11-08 18:40:31 -0500

    choice_label option: Two yes choices

--- src/AppBundle/Form/ProductType.php
+++ src/AppBundle/Form/ProductType.php
@@ -16,8 +16,17 @@ class ProductType extends AbstractType
                     'Yes' => 'stock_yes',
                     'No' => 'stock_no',
                     'Backordered' => 'stock_backordered',
+                    'yes_later' => 'stock_yes_second',
                 ],
                 'choices_as_values' => true,
+                'choice_label' => function($choiceVal, $choiceKey, $choiceIndex) {
+                    if ($choiceKey == 'yes_later') {
+                        // we're purposefully trying to have *two* yes'es
+                        return 'Yes';
+                    }
+
+                    return $choiceKey;
+                },
             ]);
     }
 }
